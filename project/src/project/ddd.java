e77e75e565dfe49e1605d7d1f275394b4957f6d6
smhrd <smhrd@211.228.63.159>
2021-05-07 11:13:38 +0900

    충돌테스트

--- project/src/project/ddd.java
+++ project/src/project/ddd.java
@@ -5,7 +5,8 @@ public class ddd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("수정");
-		
+		System.out.println("클릭");
+		System.out.print("충돌");
 		System.out.print("충돌");
 	}
 
